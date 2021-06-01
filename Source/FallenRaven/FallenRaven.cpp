6a8d3969e0543a841b983b55e3b72b9c28c6d48a
Mikołaj Nawrocki <m.nawrocki7932gmail.com>
2021-06-01 22:35:13 +0200

    28. Weapons v1.1
    + Added Weapon to Inventory

---
+++ Source/FallenRaven/FallenRaven.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FallenRaven.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FallenRaven, "FallenRaven" );
