bf1e8c776d8e1927b56ab401d868d7dc3ddea1da
Mikołaj Nawrocki <m.nawrocki7932gmail.com>
2021-06-05 00:51:08 +0200

    29. Weapons v1.2
    + Added all weapons actors
    + Added all weapons Data Assets
    + Added all weapons to sockets
    - Change EQ Icon

--- Source/FallenRaven/DA_WeaponInfo.h
+++ Source/FallenRaven/DA_WeaponInfo.h
@@ -4,14 +4,38 @@
 
 #include "CoreMinimal.h"
 #include "Engine/DataAsset.h"
+#include "Engine/UserDefinedEnum.h"
 #include "DA_WeaponInfo.generated.h"
 
-/**
- * 
- */
+UENUM(BlueprintType)
+enum class Hand : uint8
+{
+	RightHand = 0 UMETA(Displayname = "RightHand"),
+	LeftHand = 1 UMETA(Displayname = "LeftHand")
+
+};
+
 UCLASS()
 class FALLENRAVEN_API UDA_WeaponInfo : public UDataAsset
 {
 	GENERATED_BODY()
-	
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float Damage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float Speed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	bool bIsEquipped;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float CharacterSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float CharacterStamina;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	Hand HandType;
 };
